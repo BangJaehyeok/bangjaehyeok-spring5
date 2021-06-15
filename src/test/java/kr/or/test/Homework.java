bd47ceef63fc78ab4f6c4f2bf79ee90b283dfe72
Family <Family@DESKTOP-ICRK20Q>
2021-06-16 02:06:59 +0900

    210615 commit

---
+++ src/test/java/kr/or/test/Homework.java
@@ -0,0 +1,18 @@
+package kr.or.test;
+
+import javax.swing.JOptionPane;
+
+public class Homework {
+
+	public static void main(String[] args) {
+		sumall();
+	}
+	
+	public static void sumall() {
+		for(int i=1;i<101;i++) {
+		System.out.println();
+		}
+	}
+
+
+}

1b1965a76236596cb204547975d63d47d048fccd
User <User@DESKTOP-KONC19A>
2021-06-18 12:38:26 +0900

    4교시 커밋

--- src/main/java/com/edu/dao/IF_BoardDAO.java
+++ src/main/java/com/edu/dao/IF_BoardDAO.java
@@ -2,6 +2,7 @@ package com.edu.dao;
 
 import java.util.List;
 
+import com.edu.vo.AttachVO;
 import com.edu.vo.BoardVO;
 import com.edu.vo.PageVO;
 
@@ -11,6 +12,15 @@ import com.edu.vo.PageVO;
  *
  */
 public interface IF_BoardDAO {
+	//첨부파일 CRUD 아래
+	public void deleteAttachAll(Integer bno) throws Exception;
+	public void deleteAttach(String save_file_name) throws Exception;
+	public void updateAttach(AttachVO attachVO) throws Exception;
+	public void insertAttach(AttachVO attachVO) throws Exception;
+	public List<AttachVO> readAttach(Integer bno) throws Exception;
+	//게시물 상세보기시 조회수 올리는 메서드(아래)
+	public void updateViewCount(Integer bno) throws Exception;
+	//페이징없는 검색된(board_type포함된) 게시물개수
 	public int countBoard(PageVO pageVO) throws Exception;
 	//기본CRUD(아래)
 	public void deleteBoard(int bno) throws Exception;

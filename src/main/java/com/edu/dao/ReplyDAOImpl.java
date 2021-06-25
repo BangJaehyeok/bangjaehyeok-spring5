acb13e821d220508092c7b3e96ec7230764b78ff
Family <Family@DESKTOP-ICRK20Q>
2021-06-25 16:41:04 +0900

    7교시 커밋

---
+++ src/main/java/com/edu/dao/ReplyDAOImpl.java
@@ -0,0 +1,64 @@
+package com.edu.dao;
+
+import java.util.List;
+
+import javax.inject.Inject;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.stereotype.Repository;
+
+import com.edu.vo.PageVO;
+import com.edu.vo.ReplyVO;
+
+/*
+ * 이 클래스는 sqlSession템플릿을 이용해서 쿼리를 실행하는 템플릿입니다.
+ * 방재혁
+ */
+@Repository
+public class ReplyDAOImpl implements IF_ReplyDAO{
+	@Inject //자바8부터 지원됨, 이전에는 @Autowired, @Resource를 썼음
+	private SqlSession sqlSession;
+	
+	@Override
+	public void deleteReplyAll(Integer bno) throws Exception {
+		// sqlSession템플릿 사용("매퍼쿼리명","매개변수명")
+		
+	}
+
+	@Override
+	public void deleteReply(Integer rno) throws Exception {
+		// sqlSession템플릿 사용("매퍼쿼리명","매개변수명")
+		
+	}
+
+	@Override
+	public void updateReply(ReplyVO replyVO) throws Exception {
+		// sqlSession템플릿 사용("매퍼쿼리명","매개변수명")
+		
+	}
+
+	@Override
+	public void replyCountUpdate(Integer bno, int count) throws Exception {
+		// 매개변수가 2개일때 처리
+		
+	}
+
+	@Override
+	public void insertReply(ReplyVO replyVO) throws Exception {
+		// sqlSession템플릿 사용("매퍼쿼리명","매개변수명")
+		sqlSession.insert("replyMapper.insertReply", replyVO);
+	}
+
+	@Override
+	public int countReply(Integer bno) throws Exception {
+		// sqlSession템플릿 사용("매퍼쿼리명","매개변수명")
+		return sqlSession.selectOne("replyMapper.countReply", bno);
+	}
+
+	@Override
+	public List<ReplyVO> selectReply(PageVO pageVO) throws Exception {
+		// sqlSession템플릿 사용("매퍼쿼리명","매개변수명")
+		return sqlSession.selectList("replyMapper.selectReply", pageVO);
+	}
+
+}
